447359cbd928f73ec69611effc6635d9b5c36c1f
Stuart Herbert <stuart@stuartherbert.com>
2021-08-06 11:17:58 +0100

    New: initial support for the `unknown` keyword

---
+++ src/latest/__testsuite__/TypescriptHandbook/BasicTypes/BasicFunction-021-input.ts
@@ -0,0 +1,5 @@
+// this test proves that we support `unknown` as a parameter type
+
+function doSomething(a: unknown) {
+    // ...
+}
--- src/latest/__testsuite__/TypescriptHandbook/BasicTypes/BasicFunction-021-intermediate.ts
+++ src/latest/__testsuite__/TypescriptHandbook/BasicTypes/BasicFunction-021-intermediate.ts
@@ -32,17 +32,35 @@
 // POSSIBILITY OF SUCH DAMAGE.
 //
 
-import { SyntaxKind } from "typescript";
+import {
+    IntermediateKind,
+    IntermediateSourceFile
+} from "../../../IntermediateTypes";
 
-export const BUILT_IN_TYPES: string[] = [];
-BUILT_IN_TYPES[SyntaxKind.AnyKeyword] = "any";
-BUILT_IN_TYPES[SyntaxKind.BigIntKeyword] = "bigint";
-BUILT_IN_TYPES[SyntaxKind.BooleanKeyword] = "boolean";
-BUILT_IN_TYPES[SyntaxKind.NeverKeyword] = "never";
-BUILT_IN_TYPES[SyntaxKind.NullKeyword] = "null";
-BUILT_IN_TYPES[SyntaxKind.NumberKeyword] = "number";
-BUILT_IN_TYPES[SyntaxKind.ObjectKeyword] = "object";
-BUILT_IN_TYPES[SyntaxKind.StringKeyword] = "string";
-BUILT_IN_TYPES[SyntaxKind.UndefinedKeyword] = "undefined";
-BUILT_IN_TYPES[SyntaxKind.UnknownKeyword] = "unknown";
-BUILT_IN_TYPES[SyntaxKind.VoidKeyword] = "void";
+const expectedResult: IntermediateSourceFile = {
+    children: [
+        {
+            kind: IntermediateKind.IntermediateFunction,
+            declared: false,
+            name: "doSomething",
+            typeParameters: [],
+            parameters: [
+                {
+                    kind: IntermediateKind.IntermediateTypedCallableParameter,
+                    paramName: "a",
+                    optional: false,
+                    typeRef: {
+                        kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                        typeName: "unknown",
+                    },
+                    initializer: undefined,
+                },
+            ],
+            returnType: undefined,
+            hasBody: true,
+        },
+    ],
+    kind: IntermediateKind.IntermediateSourceFile,
+}
+
+export default expectedResult;
