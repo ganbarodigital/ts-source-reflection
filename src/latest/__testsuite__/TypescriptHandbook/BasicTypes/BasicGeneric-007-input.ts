a02b5ee9c89dd16d0770b4bab5b2aab6896d7b9f
Stuart Herbert <stuart@stuartherbert.com>
2021-08-11 07:41:28 +0100

    Tests: prove that type aliases can be a mix of generic and non-generic types

---
+++ src/latest/__testsuite__/TypescriptHandbook/BasicTypes/BasicGeneric-007-input.ts
@@ -0,0 +1,7 @@
+type OrNull<Type> = Type | null;
+
+type OneOrMany<Type> = Type | Type[];
+
+type OneOrManyOrNull<Type> = OrNull<OneOrMany<Type>>;
+
+type OneOrManyOrNullStrings = OneOrManyOrNull<string>;
