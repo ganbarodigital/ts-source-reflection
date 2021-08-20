b339f8c6118cf4463752e450d2104f83523d4ff7
Stuart Herbert <stuart@stuartherbert.com>
2021-08-20 08:08:43 +0100

    Tests: prove that we can use typeof with overloaded function signatures

---
+++ src/latest/__testsuite__/TypescriptHandbook/BasicTypes/ConditionalTypes-007-input.ts
@@ -0,0 +1,5 @@
+declare function stringOrNum(x: string): number;
+declare function stringOrNum(x: number): string;
+declare function stringOrNum(x: string | number): string | number;
+
+type T1 = ReturnType<typeof stringOrNum>;
