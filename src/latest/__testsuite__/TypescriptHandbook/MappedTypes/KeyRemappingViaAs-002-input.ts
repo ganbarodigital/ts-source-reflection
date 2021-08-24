63c27091197629e1c35c521df4b8b56c11fa58bd
Stuart Herbert <stuart@stuartherbert.com>
2021-08-24 11:58:19 +0100

    Tests: added 2nd KeyMappingViaAs test case

---
+++ src/latest/__testsuite__/TypescriptHandbook/MappedTypes/KeyRemappingViaAs-002-input.ts
@@ -0,0 +1,11 @@
+// Remove the 'kind' property
+type RemoveKindField<Type> = {
+    [Property in keyof Type as Exclude<Property, "kind">]: Type[Property]
+};
+
+interface Circle {
+    kind: "circle";
+    radius: number;
+}
+
+type KindlessCircle = RemoveKindField<Circle>;
