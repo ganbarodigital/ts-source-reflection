9f0b3231373780267876f2ead639aa759177fea8
Stuart Herbert <stuart@stuartherbert.com>
2021-08-25 09:31:09 +0100

    Tests: added 1st InferenceWithTemplateLiterals test case

---
+++ src/latest/__testsuite__/TypescriptHandbook/TemplateLiteralTypes/InferenceWithTemplateLiterals-001-input.ts
@@ -0,0 +1,6 @@
+type PropEventSource<Type> = {
+    on<Key extends string & keyof Type>
+        (eventName: `${Key}Changed`, callback: (newValue: Type[Key]) => void ): void;
+};
+
+declare function makeWatchedObject<Type>(obj: Type): Type & PropEventSource<Type>;
---
+++ src/latest/__testsuite__/TypescriptHandbook/TemplateLiteralTypes/InferenceWithTemplateLiterals-001-intermediate.ts
@@ -0,0 +1,203 @@
+//
+// Copyright (c) 2021-present Ganbaro Digital Ltd
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//   * Re-distributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//   * Neither the names of the copyright holders nor the names of his
+//     contributors may be used to endorse or promote products derived
+//     from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+
+import {
+    IntermediateKind,
+    IntermediateSourceFile
+} from "../../../IntermediateTypes";
+
+const expectedResult: IntermediateSourceFile = {
+    children: [
+        {
+            kind: IntermediateKind.IntermediateTypeAliasDefinition,
+            typeName: "PropEventSource",
+            typeParameters: [
+                {
+                    kind: IntermediateKind.IntermediateGenericType,
+                    name: "Type",
+                    constraint: undefined,
+                    defaultType: undefined,
+                },
+            ],
+            typeRef: {
+                kind: IntermediateKind.IntermediateAnonymousClassType,
+                members: [
+                    {
+                        kind: IntermediateKind.IntermediateMethodSignature,
+                        docBlock: undefined,
+                        accessModifier: undefined,
+                        static: false,
+                        typeParameters: [
+                            {
+                                kind: IntermediateKind.IntermediateGenericType,
+                                name: "Key",
+                                constraint: {
+                                    kind: IntermediateKind.IntermediateIntersectionType,
+                                    typeRefs: [
+                                        {
+                                            kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                                            typeName: "string",
+                                        },
+                                        {
+                                            kind: IntermediateKind.IntermediateKeyofTypeReference,
+                                            typeRef: {
+                                                kind: IntermediateKind.IntermediateFixedTypeReference,
+                                                typeName: "Type",
+                                            },
+                                        },
+                                    ],
+                                },
+                                defaultType: undefined,
+                            },
+                        ],
+                        name: "on",
+                        parameters: [
+                            {
+                                kind: IntermediateKind.IntermediateTypedCallableParameter,
+                                paramName: "eventName",
+                                readonly: false,
+                                optional: false,
+                                typeRef: {
+                                    kind: IntermediateKind.IntermediateTemplateLiteralType,
+                                    head: "",
+                                    spans: [
+                                        {
+                                            kind: IntermediateKind.IntermediateTemplateLiteralSpan,
+                                            typeRef: {
+                                                kind: IntermediateKind.IntermediateFixedTypeReference,
+                                                typeName: "Key",
+                                            },
+                                            tail: "Changed",
+                                        },
+                                    ],
+                                },
+                                initializer: undefined,
+                            },
+                            {
+                                kind: IntermediateKind.IntermediateTypedCallableParameter,
+                                paramName: "callback",
+                                readonly: false,
+                                optional: false,
+                                typeRef: {
+                                    kind: IntermediateKind.IntermediateFunctionTypeSignature,
+                                    typeParameters: [],
+                                    parameters: [
+                                        {
+                                            kind: IntermediateKind.IntermediateTypedCallableParameter,
+                                            paramName: "newValue",
+                                            readonly: false,
+                                            optional: false,
+                                            typeRef: {
+                                                kind: IntermediateKind.IntermediateIndexedAccessTypeReference,
+                                                typeRef: {
+                                                    kind: IntermediateKind.IntermediateFixedTypeReference,
+                                                    typeName: "Type",
+                                                },
+                                                indexRef: {
+                                                    kind: IntermediateKind.IntermediateFixedTypeReference,
+                                                    typeName: "Key",
+                                                },
+                                            },
+                                            initializer: undefined,
+                                        },
+                                    ],
+                                    returnType: {
+                                        kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                                        typeName: "void",
+                                    }
+                                },
+                                initializer: undefined,
+                            }
+                        ],
+                        returnType: {
+                            kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                            typeName: "void",
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            kind: IntermediateKind.IntermediateFunction,
+            docBlock: undefined,
+            declared: true,
+            exported: false,
+            name: "makeWatchedObject",
+            typeParameters: [
+                {
+                    kind: IntermediateKind.IntermediateGenericType,
+                    name: "Type",
+                    constraint: undefined,
+                    defaultType: undefined,
+                },
+            ],
+            parameters: [
+                {
+                    kind: IntermediateKind.IntermediateTypedCallableParameter,
+                    paramName: "obj",
+                    readonly: false,
+                    optional: false,
+                    typeRef: {
+                        kind: IntermediateKind.IntermediateFixedTypeReference,
+                        typeName: "Type",
+                    },
+                    initializer: undefined,
+                }
+            ],
+            returnType: {
+                kind: IntermediateKind.IntermediateIntersectionType,
+                typeRefs: [
+                    {
+                        kind: IntermediateKind.IntermediateFixedTypeReference,
+                        typeName: "Type",
+                    },
+                    {
+                        kind: IntermediateKind.IntermediateGenericTypeReference,
+                        typeName: "PropEventSource",
+                        typeArguments: [
+                            {
+                                kind: IntermediateKind.IntermediateFixedTypeReference,
+                                typeName: "Type",
+                            },
+                        ],
+                    },
+                ],
+            },
+            hasBody: false,
+        }
+    ],
+    kind: IntermediateKind.IntermediateSourceFile,
+}
+
+export default expectedResult;
