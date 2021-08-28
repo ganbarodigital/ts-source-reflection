c28cfd3f590a70fe714189972dc13f4388485e05
Stuart Herbert <stuart@stuartherbert.com>
2021-08-28 14:34:26 +0100

    Tests: added 3rd MemberVisibility test case

---
+++ src/latest/__testsuite__/TypescriptHandbook/Classes/MemberVisibility-003-intermediate.ts
@@ -0,0 +1,153 @@
+//
+// Copyright (c) 2021-present Ganbaro Digital Ltd
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//   * Re-distributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//   * Neither the names of the copyright holders nor the names of his
+//     contributors may be used to endorse or promote products derived
+//     from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+
+import {
+    IntermediateKind,
+    IntermediateRestrictableScope,
+    IntermediateSourceFile
+} from "../../../IntermediateTypes";
+
+const expectedResult: IntermediateSourceFile = {
+    children: [
+        {
+            kind: IntermediateKind.IntermediateClass,
+            docBlock: undefined,
+            declared: false,
+            exported: false,
+            typeParameters: [],
+            name: "Base",
+            extends: [],
+            implements: [],
+            members: [
+                {
+                    kind: IntermediateKind.IntermediateUntypedPropertyDefinition,
+                    accessModifier: IntermediateRestrictableScope.PROTECTED,
+                    propName: "m",
+                    propIsOptional: false,
+                    propIsReadonly: false,
+                    initializer: {
+                        kind: IntermediateKind.IntermediateNumericLiteral,
+                        value: "10",
+                        typeAssertion: undefined,
+                        asType: undefined,
+                    },
+                },
+            ],
+        },
+        {
+            kind: IntermediateKind.IntermediateClass,
+            docBlock: undefined,
+            declared: false,
+            exported: false,
+            name: "Derived",
+            extends: [
+                {
+                    kind: IntermediateKind.IntermediateFixedTypeArgument,
+                    name: "Base",
+                },
+            ],
+            implements: [],
+            typeParameters: [],
+            members: [
+                {
+                    kind: IntermediateKind.IntermediateUntypedPropertyDefinition,
+                    accessModifier: undefined,
+                    propName: "m",
+                    propIsOptional: false,
+                    propIsReadonly: false,
+                    initializer: {
+                        kind: IntermediateKind.IntermediateNumericLiteral,
+                        value: "15",
+                        typeAssertion: undefined,
+                        asType: undefined,
+                    },
+                },
+            ],
+        },
+        {
+            kind: IntermediateKind.IntermediateVariableDeclarations,
+            variables: [
+                {
+                    kind: IntermediateKind.IntermediateVariableDeclaration,
+                    docBlock: undefined,
+                    declared: false,
+                    exported: false,
+                    readonly: false,
+                    constant: true,
+                    variableName: "d",
+                    typeRef: undefined,
+                    initializer: {
+                        kind: IntermediateKind.IntermediateNewExpression,
+                        typeRef: {
+                            kind: IntermediateKind.IntermediateFixedTypeReference,
+                            typeName: "Derived",
+                        },
+                        arguments: [],
+                    },
+                },
+            ],
+        },
+        {
+            kind: IntermediateKind.IntermediateCallExpression,
+            expression: {
+                kind: IntermediateKind.IntermediatePropertyAccessExpression,
+                target: {
+                    kind: IntermediateKind.IntermediateIdentifierReference,
+                    name: "console",
+                    typeAssertion: undefined,
+                    asType: undefined,
+                },
+                propName: "log",
+            },
+            typeArguments: [],
+            arguments: [
+                {
+                    kind: IntermediateKind.IntermediatePropertyAccessExpression,
+                    target: {
+                        kind: IntermediateKind.IntermediateIdentifierReference,
+                        name: "d",
+                        typeAssertion: undefined,
+                        asType: undefined
+                    },
+                    propName: "m",
+                },
+            ],
+            typeAssertion: undefined,
+            asType: undefined,
+        },
+    ],
+    kind: IntermediateKind.IntermediateSourceFile,
+}
+
+export default expectedResult;
