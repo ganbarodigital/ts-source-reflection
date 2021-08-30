028d39cbb869a71dbb432e3c9b4ca7560a1621a9
Stuart Herbert <stuart@stuartherbert.com>
2021-08-30 11:50:47 +0100

    New: added 1st AbstractClassSignatures test case

---
+++ src/latest/__testsuite__/TypescriptHandbook/Classes/AbstractClassSignatures-001-intermediate.ts
@@ -0,0 +1,163 @@
+//
+// Copyright (c) 2021-present Ganbaro Digital Ltd
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//   * Re-distributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//   * Neither the names of the copyright holders nor the names of his
+//     contributors may be used to endorse or promote products derived
+//     from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+
+import {
+    IntermediateKind,
+    IntermediateSourceFile
+} from "../../../IntermediateTypes";
+
+const expectedResult: IntermediateSourceFile = {
+    children: [
+        {
+            kind: IntermediateKind.IntermediateClass,
+            docBlock: undefined,
+            isDeclared: false,
+            isExported: false,
+            isAbstract: true,
+            name: "Base",
+            typeParameters: [],
+            extends: [],
+            implements: [],
+            members: [
+                {
+                    kind: IntermediateKind.IntermediateMethodDefinition,
+                    docBlock: undefined,
+                    name: "getName",
+                    accessModifier: undefined,
+                    isStatic: false,
+                    isAbstract: true,
+                    typeParameters: [],
+                    parameters: [],
+                    returnType: {
+                        kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                        typeName: "string",
+                    },
+                    hasBody: false,
+                },
+                {
+                    kind: IntermediateKind.IntermediateMethodDefinition,
+                    docBlock: undefined,
+                    name: "printName",
+                    accessModifier: undefined,
+                    isStatic: false,
+                    isAbstract: false,
+                    typeParameters: [],
+                    parameters: [],
+                    returnType: undefined,
+                    hasBody: true,
+                },
+            ],
+        },
+        {
+            kind: IntermediateKind.IntermediateClass,
+            isDeclared: false,
+            isAbstract: false,
+            isExported: false,
+            docBlock: undefined,
+            name: "Derived",
+            typeParameters: [],
+            extends: [
+                {
+                    kind: IntermediateKind.IntermediateFixedTypeArgument,
+                    name: "Base",
+                },
+            ],
+            implements: [],
+            members: [
+                {
+                    kind: IntermediateKind.IntermediateMethodDefinition,
+                    docBlock: undefined,
+                    isAbstract: false,
+                    isStatic: false,
+                    accessModifier: undefined,
+                    name: "getName",
+                    typeParameters: [],
+                    parameters: [],
+                    returnType: undefined,
+                    hasBody: true,
+                },
+            ],
+        },
+        {
+            kind: IntermediateKind.IntermediateFunction,
+            docBlock: undefined,
+            isDeclared: false,
+            isExported: false,
+            name: "greet",
+            typeParameters: [],
+            parameters: [
+                {
+                    kind: IntermediateKind.IntermediateTypedCallableParameterDefinition,
+                    name: "ctor",
+                    isOptional: false,
+                    isReadonly: false,
+                    typeRef: {
+                        kind: IntermediateKind.IntermediateConstructorDefinition,
+                        parameters: [],
+                        returnType: {
+                            kind: IntermediateKind.IntermediateFixedTypeReference,
+                            typeName: "Base",
+                        },
+                    },
+                    initializer: undefined,
+                },
+            ],
+            returnType: undefined,
+            hasBody: true,
+        },
+        {
+            kind: IntermediateKind.IntermediateCallExpression,
+            typeArguments: [],
+            expression: {
+                kind: IntermediateKind.IntermediateIdentifierReference,
+                name: "greet",
+                asType: undefined,
+                typeAssertion: undefined,
+            },
+            arguments: [
+                {
+                    kind: IntermediateKind.IntermediateIdentifierReference,
+                    name: "Derived",
+                    asType: undefined,
+                    typeAssertion: undefined,
+                },
+            ],
+            typeAssertion: undefined,
+            asType: undefined,
+        }
+    ],
+    kind: IntermediateKind.IntermediateSourceFile,
+}
+
+export default expectedResult;
