1269b4e8da8ac6ec50e325df54e2f6aec5db1f80
Stuart Herbert <stuart@stuartherbert.com>
2021-08-30 08:34:55 +0100

    Tests: added 1st ThisTypes test case

---
+++ src/latest/__testsuite__/TypescriptHandbook/Classes/ThisTypes-001-intermediate.ts
@@ -0,0 +1,189 @@
+//
+// Copyright (c) 2021-present Ganbaro Digital Ltd
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//   * Re-distributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//   * Neither the names of the copyright holders nor the names of his
+//     contributors may be used to endorse or promote products derived
+//     from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+
+import {
+    IntermediateKind,
+    IntermediateSourceFile
+} from "../../../IntermediateTypes";
+
+const expectedResult: IntermediateSourceFile = {
+    children: [
+        {
+            kind: IntermediateKind.IntermediateClass,
+            docBlock: undefined,
+            declared: false,
+            exported: false,
+            name: "Box",
+            typeParameters: [],
+            extends: [],
+            implements: [],
+            members: [
+                {
+                    kind: IntermediateKind.IntermediateTypedPropertyDefinition,
+                    accessModifier: undefined,
+                    propName: "contents",
+                    propIsOptional: false,
+                    propIsReadonly: false,
+                    propIsStatic: false,
+                    typeRef: {
+                        kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                        typeName: "string",
+                    },
+                    initializer: {
+                        kind: IntermediateKind.IntermediateStringLiteral,
+                        value: "",
+                        asType: undefined,
+                        typeAssertion: undefined,
+                    },
+                },
+                {
+                    kind: IntermediateKind.IntermediateMethodDefinition,
+                    docBlock: undefined,
+                    name: "set",
+                    accessModifier: undefined,
+                    static: false,
+                    typeParameters: [],
+                    parameters: [
+                        {
+                            kind: IntermediateKind.IntermediateTypedCallableParameterDefinition,
+                            paramName: "value",
+                            readonly: false,
+                            optional: false,
+                            typeRef: {
+                                kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                                typeName: "string",
+                            },
+                            initializer: undefined,
+                        },
+                    ],
+                    returnType: undefined,
+                    hasBody: true,
+                },
+            ],
+        },
+        {
+            kind: IntermediateKind.IntermediateClass,
+            docBlock: undefined,
+            declared: false,
+            exported: false,
+            name: "ClearableBox",
+            typeParameters: [],
+            extends: [
+                {
+                    kind: IntermediateKind.IntermediateFixedTypeArgument,
+                    name: "Box",
+                },
+            ],
+            implements: [],
+            members: [
+                {
+                    kind: IntermediateKind.IntermediateMethodDefinition,
+                    docBlock: undefined,
+                    static: false,
+                    accessModifier: undefined,
+                    name: "clear",
+                    typeParameters: [],
+                    parameters: [],
+                    returnType: undefined,
+                    hasBody: true,
+                },
+            ],
+        },
+        {
+            kind: IntermediateKind.IntermediateVariableDeclarations,
+            variables: [
+                {
+                    kind: IntermediateKind.IntermediateVariableDeclaration,
+                    docBlock: undefined,
+                    declared: false,
+                    exported: false,
+                    readonly: false,
+                    constant: true,
+                    variableName: "a",
+                    typeRef: undefined,
+                    initializer: {
+                        kind: IntermediateKind.IntermediateNewExpression,
+                        typeRef: {
+                            kind: IntermediateKind.IntermediateFixedTypeReference,
+                            typeName: "ClearableBox",
+                        },
+                        arguments: [],
+                    },
+                },
+            ],
+        },
+        {
+            kind: IntermediateKind.IntermediateVariableDeclarations,
+            variables: [
+                {
+                    kind: IntermediateKind.IntermediateVariableDeclaration,
+                    docBlock: undefined,
+                    declared: false,
+                    exported: false,
+                    readonly: false,
+                    constant: true,
+                    variableName: "b",
+                    typeRef: undefined,
+                    initializer: {
+                        kind: IntermediateKind.IntermediateCallExpression,
+                        expression: {
+                            kind: IntermediateKind.IntermediatePropertyAccessExpression,
+                            target: {
+                                kind: IntermediateKind.IntermediateIdentifierReference,
+                                name: "a",
+                                typeAssertion: undefined,
+                                asType: undefined,
+                            },
+                            propName: "set",
+                        },
+                        typeArguments: [],
+                        arguments: [
+                            {
+                                kind: IntermediateKind.IntermediateStringLiteral,
+                                value: "hello",
+                                typeAssertion: undefined,
+                                asType: undefined,
+                            },
+                        ],
+                        asType: undefined,
+                        typeAssertion: undefined,
+                    },
+                },
+            ],
+        },
+    ],
+    kind: IntermediateKind.IntermediateSourceFile,
+}
+
+export default expectedResult;
