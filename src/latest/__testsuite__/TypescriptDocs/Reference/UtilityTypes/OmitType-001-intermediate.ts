c037d2c618a8c5d403a4f9e816890c1db2d8b5de
Stuart Herbert <stuart@stuartherbert.com>
2021-09-02 07:59:51 +0100

    Tests: added 1st OmitType test case

---
+++ src/latest/__testsuite__/TypescriptDocs/Reference/UtilityTypes/OmitType-001-intermediate.ts
@@ -0,0 +1,176 @@
+//
+// Copyright (c) 2021-present Ganbaro Digital Ltd
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//   * Re-distributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//   * Neither the names of the copyright holders nor the names of his
+//     contributors may be used to endorse or promote products derived
+//     from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+
+import {
+    IntermediateKind,
+    IntermediateSourceFile
+} from "../../../../IntermediateTypes";
+
+const expectedResult: IntermediateSourceFile = {
+    children: [
+        {
+            kind: IntermediateKind.IntermediateInterface,
+            docBlock: undefined,
+            isDeclared: false,
+            isDefaultExport: false,
+            isExported: false,
+            name: "Todo",
+            typeParameters: [],
+            extends: [],
+            members: [
+                {
+                    kind: IntermediateKind.IntermediateTypedPropertySignature,
+                    name: "title",
+                    isOptional: false,
+                    isReadonly: false,
+                    typeRef: {
+                        kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                        typeName: "string",
+                    },
+                },
+                {
+                    kind: IntermediateKind.IntermediateTypedPropertySignature,
+                    name: "description",
+                    isOptional: false,
+                    isReadonly: false,
+                    typeRef: {
+                        kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                        typeName: "string",
+                    },
+                },
+                {
+                    kind: IntermediateKind.IntermediateTypedPropertySignature,
+                    name: "completed",
+                    isOptional: false,
+                    isReadonly: false,
+                    typeRef: {
+                        kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                        typeName: "boolean",
+                    },
+                },
+                {
+                    kind: IntermediateKind.IntermediateTypedPropertySignature,
+                    name: "createdAt",
+                    isOptional: false,
+                    isReadonly: false,
+                    typeRef: {
+                        kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                        typeName: "number",
+                    },
+                },
+            ],
+        },
+        {
+            kind: IntermediateKind.IntermediateTypeAliasDefinition,
+            docBlock: undefined,
+            isDefaultExport: false,
+            isExported: false,
+            name: "TodoPreview",
+            typeParameters: [],
+            typeRef: {
+                kind: IntermediateKind.IntermediateGenericTypeReference,
+                typeName: "Omit",
+                typeArguments: [
+                    {
+                        kind: IntermediateKind.IntermediateFixedTypeReference,
+                        typeName: "Todo",
+                    },
+                    {
+                        kind: IntermediateKind.IntermediateLiteralType,
+                        typeName: '"description"',
+                    },
+                ],
+            },
+        },
+        {
+            kind: IntermediateKind.IntermediateVariableDeclarations,
+            variables: [
+                {
+                    kind: IntermediateKind.IntermediateConstDeclaration,
+                    docBlock: undefined,
+                    isConstant: true,
+                    isDeclared: false,
+                    isDefaultExport: false,
+                    isExported: false,
+                    isReadonly: false,
+                    name: "todo",
+                    typeRef: {
+                        kind: IntermediateKind.IntermediateFixedTypeReference,
+                        typeName: "TodoPreview",
+                    },
+                    initializer: {
+                        kind: IntermediateKind.IntermediateObjectLiteral,
+                        properties: [
+                            {
+                                kind: IntermediateKind.IntermediatePropertyAssignment,
+                                propertyName: "title",
+                                initializer: {
+                                    kind: IntermediateKind.IntermediateStringLiteral,
+                                    value: "Clean room",
+                                    asType: undefined,
+                                    typeAssertion: undefined,
+                                },
+                            },
+                            {
+                                kind: IntermediateKind.IntermediatePropertyAssignment,
+                                propertyName: "completed",
+                                initializer: {
+                                    kind: IntermediateKind.IntermediateBooleanLiteral,
+                                    value: "false",
+                                    asType: undefined,
+                                    typeAssertion: undefined,
+                                },
+                            },
+                            {
+                                kind: IntermediateKind.IntermediatePropertyAssignment,
+                                propertyName: "createdAt",
+                                initializer: {
+                                    kind: IntermediateKind.IntermediateNumericLiteral,
+                                    value: "1615544252770",
+                                    asType: undefined,
+                                    typeAssertion: undefined,
+                                },
+                            },
+                        ],
+                        typeAssertion: undefined,
+                        asType: undefined,
+                    },
+                },
+            ],
+        },
+    ],
+    kind: IntermediateKind.IntermediateSourceFile,
+}
+
+export default expectedResult;
