b67caf8a2f1c35d3fd15c3675cf38011c295ce2a
Stuart Herbert <stuart@stuartherbert.com>
2021-09-05 10:06:55 +0100

    Tests: added 2nd MergingNamespacesWithClasses test case

---
+++ src/latest/__testsuite__/TypescriptDocs/Reference/DeclarationMerging/MergingNamespacesWithClasses-002-intermediate.ts
@@ -0,0 +1,127 @@
+//
+// Copyright (c) 2021-present Ganbaro Digital Ltd
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//   * Re-distributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//   * Neither the names of the copyright holders nor the names of his
+//     contributors may be used to endorse or promote products derived
+//     from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+
+import {
+    IntermediateKind, IntermediateSourceFile
+} from "../../../../IntermediateTypes";
+
+const expectedResult: IntermediateSourceFile = {
+    children: [
+        {
+            kind: IntermediateKind.IntermediateFunction,
+            docBlock: undefined,
+            isDeclared: false,
+            isDefaultExport: false,
+            isExported: false,
+            name: "buildLabel",
+            typeParameters: [],
+            parameters: [
+                {
+                    kind: IntermediateKind.IntermediateTypedCallableParameterDeclaration,
+                    decorators: [],
+                    isOptional: false,
+                    isReadonly: false,
+                    name: "name",
+                    typeRef: {
+                        kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                        typeName: "string",
+                    },
+                    initializer: undefined,
+                },
+            ],
+            returnType: {
+                kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                typeName: "string",
+            },
+            hasBody: true,
+        },
+        {
+            kind: IntermediateKind.IntermediateNamespace,
+            docBlock: undefined,
+            isDeclared: false,
+            isDefaultExport: false,
+            isExported: false,
+            name: "buildLabel",
+            children: [
+                {
+                    kind: IntermediateKind.IntermediateVariableDeclarations,
+                    variables: [
+                        {
+                            kind: IntermediateKind.IntermediateLetDeclaration,
+                            isConstant: false,
+                            isDeclared: false,
+                            isDefaultExport: false,
+                            isExported: true,
+                            isReadonly: false,
+                            docBlock: undefined,
+                            name: "suffix",
+                            typeRef: undefined,
+                            initializer: {
+                                kind: IntermediateKind.IntermediateStringLiteral,
+                                value: "",
+                                asType: undefined,
+                                typeAssertion: undefined,
+                            },
+                        },
+                    ],
+                },
+                {
+                    kind: IntermediateKind.IntermediateVariableDeclarations,
+                    variables: [
+                        {
+                            kind: IntermediateKind.IntermediateLetDeclaration,
+                            isConstant: false,
+                            isDeclared: false,
+                            isDefaultExport: false,
+                            isExported: true,
+                            isReadonly: false,
+                            docBlock: undefined,
+                            name: "prefix",
+                            typeRef: undefined,
+                            initializer: {
+                                kind: IntermediateKind.IntermediateStringLiteral,
+                                value: "Hello, ",
+                                asType: undefined,
+                                typeAssertion: undefined,
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+    kind: IntermediateKind.IntermediateSourceFile,
+}
+
+export default expectedResult;
