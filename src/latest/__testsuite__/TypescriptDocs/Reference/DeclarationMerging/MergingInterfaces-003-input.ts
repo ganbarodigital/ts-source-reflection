769ab05fcc6010abcb718bcc8d0339f82441d512
Stuart Herbert <stuart@stuartherbert.com>
2021-09-05 08:26:36 +0100

    Tests: added 3rd MergingInterfaces test case

---
+++ src/latest/__testsuite__/TypescriptDocs/Reference/DeclarationMerging/MergingInterfaces-003-input.ts
@@ -0,0 +1,11 @@
+interface Document {
+    createElement(tagName: any): Element;
+}
+interface Document {
+    createElement(tagName: "div"): HTMLDivElement;
+    createElement(tagName: "span"): HTMLSpanElement;
+}
+interface Document {
+    createElement(tagName: string): HTMLElement;
+    createElement(tagName: "canvas"): HTMLCanvasElement;
+}
