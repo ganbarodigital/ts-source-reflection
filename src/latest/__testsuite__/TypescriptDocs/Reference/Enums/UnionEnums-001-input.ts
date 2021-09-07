fbd61755e78dc3956b29650f8427ccb59657ffa2
Stuart Herbert <stuart@stuartherbert.com>
2021-09-07 08:10:27 +0100

    Tests: added 1st UnionEnums test case

---
+++ src/latest/__testsuite__/TypescriptDocs/Reference/Enums/UnionEnums-001-input.ts
@@ -0,0 +1,14 @@
+enum ShapeKind {
+    Circle,
+    Square,
+}
+
+interface Circle {
+    kind: ShapeKind.Circle;
+    radius: number;
+}
+
+interface Square {
+    kind: ShapeKind.Square;
+    sideLength: number;
+}
