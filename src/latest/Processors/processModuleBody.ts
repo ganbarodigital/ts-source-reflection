dde9d18385e82bf05f4df9c8dbf8005f0cf4320f
Stuart Herbert <stuart@stuartherbert.com>
2021-09-19 09:12:27 +0100

    New: added initial processModuleBody()

---
+++ src/latest/Processors/processModuleBody.ts
@@ -0,0 +1,76 @@
+//
+// Copyright (c) 2021-present Ganbaro Digital Ltd
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//   * Re-distributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//   * Neither the names of the copyright holders nor the names of his
+//     contributors may be used to endorse or promote products derived
+//     from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+
+import { DEFAULT_DATA_PATH, getClassNames, UnsupportedTypeError } from "@safelytyped/core-types";
+import { isModuleBlock, ModuleBlock, ModuleBody } from "typescript";
+import {
+    IntermediateKind,
+    IntermediateModuleBlock,
+    IntermediateModuleContents
+} from "../IntermediateTypes";
+import { processStatements } from "./processStatements";
+
+export function processModuleBody(
+    input: ModuleBody
+): IntermediateModuleContents
+{
+    // what kind of module body do we have?
+    if (isModuleBlock(input)) {
+        return processModuleBlock(input);
+    }
+
+    // add other kinds here
+
+    // if we get here, then we've got a module body that we don't
+    // have a test case for
+
+    // tslint:disable-next-line: no-console
+    console.log(getClassNames(input));
+    throw new UnsupportedTypeError({
+        public: {
+            dataPath: DEFAULT_DATA_PATH,
+            expected: "a supported module body",
+            actual: getClassNames(input)[0],
+        }
+    });
+}
+
+function processModuleBlock(
+    input: ModuleBlock
+): IntermediateModuleBlock {
+    return {
+        kind: IntermediateKind.IntermediateModuleBlock,
+        children: processStatements(input.statements),
+    }
+}
