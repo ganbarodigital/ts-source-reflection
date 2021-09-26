0d1b7d490f037ab04013e3cac6ec1df8ac4e2f7c
Stuart Herbert <stuart@stuartherbert.com>
2021-09-26 09:51:17 +0100

    Tests: added 2nd Symbols test case

---
+++ src/latest/__testsuite__/TypescriptDocs/Reference/Symbols/Symbols-002-input.ts
@@ -0,0 +1,3 @@
+let sym2 = Symbol("key");
+let sym3 = Symbol("key");
+sym2 === sym3; // false, symbols are unique
---
+++ src/latest/__testsuite__/TypescriptDocs/Reference/Symbols/Symbols-002-intermediate.ts
@@ -0,0 +1,136 @@
+//
+// Copyright (c) 2021-present Ganbaro Digital Ltd
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//   * Re-distributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//   * Neither the names of the copyright holders nor the names of his
+//     contributors may be used to endorse or promote products derived
+//     from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+
+import {
+    IntermediateExpressionOperator,
+    IntermediateKind,
+    IntermediateSourceFile
+} from "../../../../IntermediateTypes";
+
+const expectedResult: IntermediateSourceFile = {
+    children: [
+        {
+            kind: IntermediateKind.IntermediateVariableDeclarations,
+            isDeclared: false,
+            isDefaultExport: false,
+            isExported: false,
+            variables: [
+                {
+                    kind: IntermediateKind.IntermediateLetDeclaration,
+                    docBlock: undefined,
+                    isConstant: false,
+                    isReadonly: false,
+                    name: "sym2",
+                    typeRef: undefined,
+                    initializer: {
+                        kind: IntermediateKind.IntermediateCallExpression,
+                        expression: {
+                            kind: IntermediateKind.IntermediateIdentifierReference,
+                            name: "Symbol",
+                            asType: undefined,
+                            typeAssertion: undefined,
+                        },
+                        typeArguments: [],
+                        arguments: [
+                            {
+                                kind: IntermediateKind.IntermediateStringLiteral,
+                                value: "key",
+                                asType: undefined,
+                                typeAssertion: undefined,
+                            },
+                        ],
+                        asType: undefined,
+                        typeAssertion: undefined,
+                    },
+                },
+            ],
+        },
+        {
+            kind: IntermediateKind.IntermediateVariableDeclarations,
+            isDeclared: false,
+            isDefaultExport: false,
+            isExported: false,
+            variables: [
+                {
+                    kind: IntermediateKind.IntermediateLetDeclaration,
+                    docBlock: undefined,
+                    isConstant: false,
+                    isReadonly: false,
+                    name: "sym3",
+                    typeRef: undefined,
+                    initializer: {
+                        kind: IntermediateKind.IntermediateCallExpression,
+                        expression: {
+                            kind: IntermediateKind.IntermediateIdentifierReference,
+                            name: "Symbol",
+                            asType: undefined,
+                            typeAssertion: undefined,
+                        },
+                        typeArguments: [],
+                        arguments: [
+                            {
+                                kind: IntermediateKind.IntermediateStringLiteral,
+                                value: "key",
+                                asType: undefined,
+                                typeAssertion: undefined,
+                            },
+                        ],
+                        asType: undefined,
+                        typeAssertion: undefined,
+                    },
+                },
+            ],
+        },
+        {
+            kind: IntermediateKind.IntermediateBinaryExpression,
+            left: {
+                kind: IntermediateKind.IntermediateIdentifierReference,
+                name: "sym2",
+                asType: undefined,
+                typeAssertion: undefined,
+            },
+            operator: IntermediateExpressionOperator.EQUALS_EQUALS_EQUALS,
+            right: {
+                kind: IntermediateKind.IntermediateIdentifierReference,
+                name: "sym3",
+                asType: undefined,
+                typeAssertion: undefined,
+            },
+        },
+    ],
+    referencedFiles: [],
+    kind: IntermediateKind.IntermediateSourceFile,
+}
+
+export default expectedResult;
