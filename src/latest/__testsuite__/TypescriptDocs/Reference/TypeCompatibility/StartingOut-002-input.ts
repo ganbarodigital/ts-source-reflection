0b4735fdd2a70220e937f9817f29d2dc4c7f9c47
Stuart Herbert <stuart@stuartherbert.com>
2021-09-27 08:34:52 +0100

    Tests: added 2nd StartingOut test case

---
+++ src/latest/__testsuite__/TypescriptDocs/Reference/TypeCompatibility/StartingOut-002-input.ts
@@ -0,0 +1,8 @@
+interface Pet {
+    name: string;
+}
+let dog = { name: "Lassie", owner: "Rudd Weatherwax" };
+function greet(pet: Pet) {
+    console.log("Hello, " + pet.name);
+}
+greet(dog); // OK
---
+++ src/latest/__testsuite__/TypescriptDocs/Reference/TypeCompatibility/StartingOut-002-intermediate.ts
@@ -0,0 +1,160 @@
+//
+// Copyright (c) 2021-present Ganbaro Digital Ltd
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//   * Re-distributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//   * Neither the names of the copyright holders nor the names of his
+//     contributors may be used to endorse or promote products derived
+//     from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+
+import {
+    IntermediateKind,
+    IntermediateSourceFile
+} from "../../../../IntermediateTypes";
+
+const expectedResult: IntermediateSourceFile = {
+    children: [
+        {
+            kind: IntermediateKind.IntermediateInterface,
+            docBlock: undefined,
+            isDeclared: false,
+            isDefaultExport: false,
+            isExported: false,
+            name: "Pet",
+            typeParameters: [],
+            extends: [],
+            members: [
+                {
+                    kind: IntermediateKind.IntermediateTypedPropertySignature,
+                    isOptional: false,
+                    isReadonly: false,
+                    name: "name",
+                    typeRef: {
+                        kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                        typeName: "string",
+                    },
+                },
+            ],
+        },
+        {
+            kind: IntermediateKind.IntermediateVariableDeclarations,
+            isDeclared: false,
+            isDefaultExport: false,
+            isExported: false,
+            variables: [
+                {
+                    kind: IntermediateKind.IntermediateLetDeclaration,
+                    docBlock: undefined,
+                    isConstant: false,
+                    isReadonly: false,
+                    name: "dog",
+                    typeRef: undefined,
+                    initializer: {
+                        kind: IntermediateKind.IntermediateObjectLiteral,
+                        properties: [
+                            {
+                                kind: IntermediateKind.IntermediatePropertyAssignment,
+                                propertyName: "name",
+                                initializer: {
+                                    kind: IntermediateKind.IntermediateStringLiteral,
+                                    value: "Lassie",
+                                    asType: undefined,
+                                    typeAssertion: undefined,
+                                },
+                            },
+                            {
+                                kind: IntermediateKind.IntermediatePropertyAssignment,
+                                propertyName: "owner",
+                                initializer: {
+                                    kind: IntermediateKind.IntermediateStringLiteral,
+                                    value: "Rudd Weatherwax",
+                                    asType: undefined,
+                                    typeAssertion: undefined,
+                                },
+                            },
+                        ],
+                        typeAssertion: undefined,
+                        asType: undefined,
+                    },
+                },
+            ],
+        },
+        {
+            kind: IntermediateKind.IntermediateFunction,
+            docBlock: undefined,
+            isDeclared: false,
+            isDefaultExport: false,
+            isExported: false,
+            name: "greet",
+            typeParameters: [],
+            parameters: [
+                {
+                    kind: IntermediateKind.IntermediateTypedCallableParameterDeclaration,
+                    decorators: [],
+                    isOptional: false,
+                    isReadonly: false,
+                    name: "pet",
+                    typeRef: {
+                        kind: IntermediateKind.IntermediateFixedTypeReference,
+                        typeName: "Pet",
+                    },
+                    initializer: undefined,
+                },
+            ],
+            returnType: undefined,
+            hasBody: true,
+        },
+        {
+            kind: IntermediateKind.IntermediateCallExpression,
+            expression: {
+                kind: IntermediateKind.IntermediateIdentifierReference,
+                name: "greet",
+                asType: undefined,
+                typeAssertion: undefined,
+            },
+            typeArguments: [],
+            arguments: [
+                {
+                    kind: IntermediateKind.IntermediateIdentifierReference,
+                    name: "dog",
+                    asType: undefined,
+                    typeAssertion: undefined,
+                },
+            ],
+            asType: undefined,
+            typeAssertion: undefined,
+        },
+    ],
+    referencedFiles: [],
+    referencedLibs: [],
+    referencedTypes: [],
+    referenceNoDefaultLib: false,
+    kind: IntermediateKind.IntermediateSourceFile,
+}
+
+export default expectedResult;
