fa768ccb9f6141992fe319da4380b2d9fcdb9b2f
Stuart Herbert <stuart@stuartherbert.com>
2021-09-28 20:38:52 +0100

    New: added mustBeTryStatement() to the AST helpers

--- src/latest/AST/Helpers/index.ts
+++ src/latest/AST/Helpers/index.ts
@@ -85,6 +85,7 @@ export * from "./mustBeInterfaceDeclaration";
 export * from "./mustBeModuleDeclaration";
 export * from "./mustBePropertyAssignment";
 export * from "./mustBePropertySignature";
+export * from "./mustBeTryStatement";
 export * from "./mustBeTypeAliasDeclaration";
 export * from "./mustBeTypeReference";
 export * from "./mustBeVariableStatement";
