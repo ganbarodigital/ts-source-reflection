00205040fef1bbbfdb4e27883189949226a9bc94
Stuart Herbert <stuart@stuartherbert.com>
2021-09-28 08:08:06 +0100

    Tests: added 1st Classes test case

---
+++ src/latest/__testsuite__/TypescriptDocs/Reference/TypeCompatibility/Classes-001-intermediate.ts
@@ -0,0 +1,238 @@
+//
+// Copyright (c) 2021-present Ganbaro Digital Ltd
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//   * Re-distributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//   * Neither the names of the copyright holders nor the names of his
+//     contributors may be used to endorse or promote products derived
+//     from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+
+import {
+    IntermediateExpressionOperator,
+    IntermediateKind,
+    IntermediateSourceFile
+} from "../../../../IntermediateTypes";
+
+const expectedResult: IntermediateSourceFile = {
+    children: [
+        {
+            kind: IntermediateKind.IntermediateClass,
+            docBlock: undefined,
+            decorators: [],
+            isAbstract: false,
+            isDeclared: false,
+            isDefaultExport: false,
+            isExported: false,
+            name: "Animal",
+            typeParameters: [],
+            extends: [],
+            implements: [],
+            members: [
+                {
+                    kind: IntermediateKind.IntermediateTypedPropertyDeclaration,
+                    docBlock: undefined,
+                    decorators: [],
+                    isOptional: false,
+                    isReadonly: false,
+                    isStatic: false,
+                    accessModifier: undefined,
+                    name: "feet",
+                    typeRef: {
+                        kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                        typeName: "number",
+                    },
+                    initializer: undefined,
+                },
+                {
+                    kind: IntermediateKind.IntermediateConstructorDeclaration,
+                    docBlock: undefined,
+                    accessModifier: undefined,
+                    parameters: [
+                        {
+                            kind: IntermediateKind.IntermediateTypedConstructorParameterDeclaration,
+                            decorators: [],
+                            isOptional: false,
+                            isReadonly: false,
+                            setsPropertyWithScope: undefined,
+                            name: "name",
+                            typeRef: {
+                                kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                                typeName: "string",
+                            },
+                            initializer: undefined,
+                        },
+                        {
+                            kind: IntermediateKind.IntermediateTypedConstructorParameterDeclaration,
+                            decorators: [],
+                            isOptional: false,
+                            isReadonly: false,
+                            setsPropertyWithScope: undefined,
+                            name: "numFeet",
+                            typeRef: {
+                                kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                                typeName: "number",
+                            },
+                            initializer: undefined,
+                        },
+                    ],
+                    returnType: undefined,
+                },
+            ],
+        },
+        {
+            kind: IntermediateKind.IntermediateClass,
+            docBlock: undefined,
+            decorators: [],
+            isAbstract: false,
+            isDeclared: false,
+            isDefaultExport: false,
+            isExported: false,
+            name: "Size",
+            typeParameters: [],
+            extends: [],
+            implements: [],
+            members: [
+                {
+                    kind: IntermediateKind.IntermediateTypedPropertyDeclaration,
+                    docBlock: undefined,
+                    decorators: [],
+                    isOptional: false,
+                    isReadonly: false,
+                    isStatic: false,
+                    accessModifier: undefined,
+                    name: "feet",
+                    typeRef: {
+                        kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                        typeName: "number",
+                    },
+                    initializer: undefined,
+                },
+                {
+                    kind: IntermediateKind.IntermediateConstructorDeclaration,
+                    docBlock: undefined,
+                    accessModifier: undefined,
+                    parameters: [
+                        {
+                            kind: IntermediateKind.IntermediateTypedConstructorParameterDeclaration,
+                            decorators: [],
+                            isOptional: false,
+                            isReadonly: false,
+                            setsPropertyWithScope: undefined,
+                            name: "numFeet",
+                            typeRef: {
+                                kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                                typeName: "number",
+                            },
+                            initializer: undefined,
+                        },
+                    ],
+                    returnType: undefined,
+                },
+            ],
+        },
+        {
+            kind: IntermediateKind.IntermediateVariableDeclarations,
+            isDeclared: false,
+            isDefaultExport: false,
+            isExported: false,
+            variables: [
+                {
+                    kind: IntermediateKind.IntermediateLetDeclaration,
+                    docBlock: undefined,
+                    isConstant: false,
+                    isReadonly: false,
+                    name: "a",
+                    typeRef: {
+                        kind: IntermediateKind.IntermediateFixedTypeReference,
+                        typeName: "Animal",
+                    },
+                    initializer: undefined,
+                },
+            ],
+        },
+        {
+            kind: IntermediateKind.IntermediateVariableDeclarations,
+            isDeclared: false,
+            isDefaultExport: false,
+            isExported: false,
+            variables: [
+                {
+                    kind: IntermediateKind.IntermediateLetDeclaration,
+                    docBlock: undefined,
+                    isConstant: false,
+                    isReadonly: false,
+                    name: "s",
+                    typeRef: {
+                        kind: IntermediateKind.IntermediateFixedTypeReference,
+                        typeName: "Size",
+                    },
+                    initializer: undefined,
+                },
+            ],
+        },
+        {
+            kind: IntermediateKind.IntermediateBinaryExpression,
+            left: {
+                kind: IntermediateKind.IntermediateIdentifierReference,
+                name: "a",
+                asType: undefined,
+                typeAssertion: undefined,
+            },
+            operator: IntermediateExpressionOperator.EQUALS,
+            right: {
+                kind: IntermediateKind.IntermediateIdentifierReference,
+                name: "s",
+                asType: undefined,
+                typeAssertion: undefined,
+            },
+        },
+        {
+            kind: IntermediateKind.IntermediateBinaryExpression,
+            left: {
+                kind: IntermediateKind.IntermediateIdentifierReference,
+                name: "s",
+                asType: undefined,
+                typeAssertion: undefined,
+            },
+            operator: IntermediateExpressionOperator.EQUALS,
+            right: {
+                kind: IntermediateKind.IntermediateIdentifierReference,
+                name: "a",
+                asType: undefined,
+                typeAssertion: undefined,
+            },
+        },
+    ],
+    referencedFiles: [],
+    referencedLibs: [],
+    referencedTypes: [],
+    referenceNoDefaultLib: false,
+    kind: IntermediateKind.IntermediateSourceFile,
+}
+
+export default expectedResult;
