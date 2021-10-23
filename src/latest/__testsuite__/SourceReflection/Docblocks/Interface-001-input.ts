dee5f387afa5d38357c86313b368201bbbaa25b4
Stuart Herbert <stuart@stuartherbert.com>
2021-10-23 11:19:02 +0100

    Tests: added 1st Docblocks/Interface test case

---
+++ src/latest/__testsuite__/SourceReflection/Docblocks/Interface-001-input.ts
@@ -0,0 +1,19 @@
+/**
+ * this is the docblock for Interface001
+ */
+interface Interface001 {
+    /**
+     * this is the docblock for prop1
+     */
+    prop1: string;
+
+    /**
+     * this is the docblock for prop2
+     */
+    prop2;
+
+    /**
+     * this is the docblock for method1
+     */
+    method1(): number;
+}
---
+++ src/latest/__testsuite__/SourceReflection/Docblocks/Interface-001-intermediate.ts
@@ -0,0 +1,103 @@
+//
+// Copyright (c) 2021-present Ganbaro Digital Ltd
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//   * Re-distributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//   * Neither the names of the copyright holders nor the names of his
+//     contributors may be used to endorse or promote products derived
+//     from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+
+import {
+    IntermediateKind, IntermediateSourceFile
+} from "../../../IntermediateTypes";
+
+const expectedResult: IntermediateSourceFile = {
+    children: [
+        {
+            kind: IntermediateKind.IntermediateInterface,
+            docBlock: {
+                kind: IntermediateKind.IntermediateDocBlock,
+                text: "/**\n * this is the docblock for Interface001\n */",
+            },
+            isDeclared: false,
+            isDefaultExport: false,
+            isExported: false,
+            name: "Interface001",
+            typeParameters: [],
+            extends: [],
+            members: [
+                {
+                    kind: IntermediateKind.IntermediateTypedPropertySignature,
+                    docBlock: {
+                        kind: IntermediateKind.IntermediateDocBlock,
+                        text: "/**\n     * this is the docblock for prop1\n     */",
+                    },
+                    isOptional: false,
+                    isReadonly: false,
+                    name: "prop1",
+                    typeRef: {
+                        kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                        typeName: "string",
+                    },
+                },
+                {
+                    kind: IntermediateKind.IntermediateUntypedPropertySignature,
+                    docBlock: {
+                        kind: IntermediateKind.IntermediateDocBlock,
+                        text: "/**\n     * this is the docblock for prop2\n     */",
+                    },
+                    isOptional: false,
+                    isReadonly: false,
+                    name: "prop2",
+                },
+                {
+                    kind: IntermediateKind.IntermediateMethodSignature,
+                    docBlock: {
+                        kind: IntermediateKind.IntermediateDocBlock,
+                        text: "/**\n     * this is the docblock for method1\n     */",
+                    },
+                    isStatic: false,
+                    name: "method1",
+                    typeParameters: [],
+                    parameters: [],
+                    returnType: {
+                        kind: IntermediateKind.IntermediateBuiltInTypeReference,
+                        typeName: "number",
+                    },
+                }
+            ],
+        }
+    ],
+    referencedFiles: [],
+    referencedLibs: [],
+    referencedTypes: [],
+    referenceNoDefaultLib: false,
+    kind: IntermediateKind.IntermediateSourceFile,
+}
+
+export default expectedResult;
