9ed97467a39c7e278d1a68790d884f4e2e57de5e
Stuart Herbert <stuart@stuartherbert.com>
2021-11-03 08:58:06 +0000

    New: initial support for return statements

---
+++ src/latest/Processors/processReturnStatement.ts
@@ -0,0 +1,59 @@
+//
+// Copyright (c) 2021-present Ganbaro Digital Ltd
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//   * Re-distributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//   * Neither the names of the copyright holders nor the names of his
+//     contributors may be used to endorse or promote products derived
+//     from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+
+import { Statement } from "typescript";
+import { AST } from "../AST";
+import { IntermediateReturnStatement, IntermediateKind } from "../IntermediateTypes";
+import { ParentContext } from "./ParentContext";
+import { processExpression } from "./processExpression";
+import { ProcessingContext } from "./ProcessingContext";
+import { processMaybe } from "./processMaybe";
+
+export function processReturnStatement(
+    processCtx: ProcessingContext,
+    parentCtx: ParentContext,
+    input: Statement
+): IntermediateReturnStatement
+{
+    // make sure we've got the statement we expect
+    const returnStmt = AST.mustBeReturnStatement(input);
+
+    return {
+        kind: IntermediateKind.IntermediateReturnStatement,
+        expression: processMaybe(
+            returnStmt.expression,
+            (value) => processExpression(processCtx, value),
+        )
+    }
+}
